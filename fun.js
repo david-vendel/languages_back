9ce9d353de57928e26ada4f6d5bc7d2b790a4b93
David Vendel <33971649+justdvl@users.noreply.github.com>
2020-04-24 23:14:47 +0200

    all-translations

---
+++ fun.js
@@ -0,0 +1,96 @@
+let Translation = require("./models/translation.js");
+const GOOGLE_TRANSLATE_API_KEY = process.env.GOOGLE_TRANSLATE_API_KEY;
+const fetch = require("node-fetch");
+
+const cachedTranslation = async (word, fromLanguage, toLanguage) => {
+  console.log("cachedTranslation");
+  if (fromLanguage === toLanguage) {
+    return [word];
+  }
+  const answer = await Translation.find({
+    word: word,
+    fromLanguage: fromLanguage,
+    toLanguage: toLanguage,
+  }).exec();
+  console.log("answer", answer);
+
+  if (answer.length === 0) {
+    const translations = await realTranslation(word, fromLanguage, toLanguage);
+    if (!translations) {
+      return false;
+    }
+    console.log("got real trans", translations.translations[0].translatedText);
+    saveTranslation(
+      word,
+      [translations.translations[0].translatedText],
+      fromLanguage,
+      toLanguage
+    );
+    return [translations.translations[0].translatedText];
+  } else {
+    const cache = await Translation.findOne({
+      word: word,
+      fromLanguage: fromLanguage,
+      toLanguage: toLanguage,
+    }).exec();
+
+    console.log("cache", cache);
+    return cache.translations;
+  }
+};
+
+const realTranslation = async (word, fromLanguage, toLanguage) => {
+  let url = `https://translation.googleapis.com/language/translate/v2?key=${GOOGLE_TRANSLATE_API_KEY}`;
+  url += "&q=" + encodeURI(word);
+  url += `&source=${fromLanguage}`;
+  url += `&target=${toLanguage}`;
+
+  console.log("url", url);
+  try {
+    return await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        console.log("response from google: ", response);
+        if (response.error) {
+          return false;
+        }
+        console.log("return", response.data.translations[0].translatedText);
+
+        return response.data;
+      })
+      .catch((error) => {
+        console.log("There was an error with the translation request: ", error);
+        return false;
+      });
+  } catch (e) {
+    console.log("translateion e", e);
+  }
+};
+
+const saveTranslation = (word, translations, fromLanguage, toLanguage) => {
+  Translation.create(
+    {
+      word: word,
+      translations: translations,
+      fromLanguage: fromLanguage,
+      toLanguage: toLanguage,
+    },
+    (err, res) => {
+      if (err) {
+        console.log("err", err);
+      } else {
+        console.log("res", res);
+      }
+    }
+  );
+};
+
+module.exports = {
+  cachedTranslation: cachedTranslation,
+};
---
+++ routes/all-translations.js
@@ -0,0 +1,154 @@
+const { ALL_TRANSLATIONS_GET } = require("./../endpoints");
+
+let express = require("express"),
+  bodyParser = require("body-parser");
+
+let index = require("./../index.js");
+let fun = require("./../fun.js");
+router = express.Router();
+
+console.log("index", index);
+console.log("fun", fun);
+
+router.use(function timeLog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+router.post(ALL_TRANSLATIONS_GET, async (req, res, next) => {
+  console.log("req body", req.body);
+  const word = req.body.word;
+  let languages = req.body.languages;
+
+  if (!languages) {
+    languages = {
+      // 'auto': 'Automatic',
+      af: "Afrikaans",
+      sq: "Albanian",
+      am: "Amharic",
+      ar: "Arabic",
+      hy: "Armenian",
+      az: "Azerbaijani",
+      eu: "Basque",
+      be: "Belarusian",
+      bn: "Bengali",
+      bs: "Bosnian",
+      bg: "Bulgarian",
+      ca: "Catalan",
+      ceb: "Cebuano",
+      ny: "Chichewa",
+      "zh-cn": "Chinese Simplified",
+      "zh-tw": "Chinese Traditional",
+      co: "Corsican",
+      hr: "Croatian",
+      cs: "Czech",
+      da: "Danish",
+      nl: "Dutch",
+      en: "English",
+      eo: "Esperanto",
+      et: "Estonian",
+      tl: "Filipino",
+      fi: "Finnish",
+      fr: "French",
+      fy: "Frisian",
+      gl: "Galician",
+      ka: "Georgian",
+      de: "German",
+      el: "Greek",
+      gu: "Gujarati",
+      ht: "Haitian Creole",
+      ha: "Hausa",
+      haw: "Hawaiian",
+      iw: "Hebrew",
+      hi: "Hindi",
+      hmn: "Hmong",
+      hu: "Hungarian",
+      is: "Icelandic",
+      ig: "Igbo",
+      id: "Indonesian",
+      ga: "Irish",
+      it: "Italian",
+      ja: "Japanese",
+      jw: "Javanese",
+      kn: "Kannada",
+      kk: "Kazakh",
+      km: "Khmer",
+      ko: "Korean",
+      ku: "Kurdish (Kurmanji)",
+      ky: "Kyrgyz",
+      lo: "Lao",
+      la: "Latin",
+      lv: "Latvian",
+      lt: "Lithuanian",
+      lb: "Luxembourgish",
+      mk: "Macedonian",
+      mg: "Malagasy",
+      ms: "Malay",
+      ml: "Malayalam",
+      mt: "Maltese",
+      mi: "Maori",
+      mr: "Marathi",
+      mn: "Mongolian",
+      my: "Myanmar (Burmese)",
+      ne: "Nepali",
+      no: "Norwegian",
+      ps: "Pashto",
+      fa: "Persian",
+      pl: "Polish",
+      pt: "Portuguese",
+      ma: "Punjabi",
+      ro: "Romanian",
+      ru: "Russian",
+      sm: "Samoan",
+      gd: "Scots Gaelic",
+      sr: "Serbian",
+      st: "Sesotho",
+      sn: "Shona",
+      sd: "Sindhi",
+      si: "Sinhala",
+      sk: "Slovak",
+      sl: "Slovenian",
+      so: "Somali",
+      es: "Spanish",
+      su: "Sundanese",
+      sw: "Swahili",
+      sv: "Swedish",
+      tg: "Tajik",
+      ta: "Tamil",
+      te: "Telugu",
+      th: "Thai",
+      tr: "Turkish",
+      uk: "Ukrainian",
+      ur: "Urdu",
+      uz: "Uzbek",
+      vi: "Vietnamese",
+      cy: "Welsh",
+      xh: "Xhosa",
+      yi: "Yiddish",
+      yo: "Yoruba",
+      zu: "Zulu",
+    };
+  }
+
+  let fromLanguage = "en";
+
+  console.log("word", word, "languages", languages);
+  let response = [];
+
+  for (let i = 0; i < Object.keys(languages).length; i++) {
+    const l = Object.keys(languages)[i];
+    const ln = Object.values(languages)[i];
+    const translationA = await fun.cachedTranslation(word, fromLanguage, l);
+    if (translationA) {
+      response.push({
+        language: l,
+        languageName: ln,
+        translation: translationA[0],
+      });
+    }
+  }
+  console.log("FINISH");
+  res.send(response);
+});
+
+module.exports = router;
